c09c1dd2b2689acc1be952eee5f798e8bcef2b17
Bastian Köcher <git@kchr.de>
2016-11-05 17:30:09 +0100

    msm8994 resets: Adds gcc resets.

---
+++ include/dt-bindings/reset/qcom,gcc-msm8994.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_RESET_MSM_GCC_8994_H
+#define _DT_BINDINGS_RESET_MSM_GCC_8994_H
+
+#define GCC_USB_30_BCR			0
+#define GCC_USB3_PHY_BCR		1
+#define GCC_USB3PHY_PHY_BCR		2
+#define GCC_USB_HS_BCR			3
+#define GCC_USB2_HS_PHY_ONLY_BCR	4
+#define GCC_USB2_PHY_BCR		5
+#define GCC_PCIE_PHY_0_PHY_BCR		6
+#define GCC_PCIE_PHY_0_BCR		7
+#define GCC_PCIE_PHY_1_PHY_BCR		8
+#define GCC_PCIE_PHY_1_BCR		9
+
+#endif
